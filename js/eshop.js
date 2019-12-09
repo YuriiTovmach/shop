42959c83e1b77ece05d311267a9139db0072ad45
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2019-12-09 16:15:09 +0200

    second commit

--- js/eshop.js
+++ js/eshop.js
@@ -1,5 +1,9 @@
+var cart = {}; //моя корзина
 $('document').ready(function(){
 	loadGoos();
+	checkCart();
+	showMiniCart();
+
 });
 
 function loadGoos(){
@@ -11,13 +15,45 @@ function loadGoos(){
 			out+='<div class="single-goods">';
 			out+='<h3>'+data[key] ['name']+'</h3>';
 			out+='<img src="'+data[key].image+'">';
-			out+='<p>Цена: '+data[key] ['cost']+" грн"+'</p>';
-			out+='<button>Купить</button>';
+			out+='<p>Цена: '+data[key] ['cost']+'</p>';
+			out+='<button class="add-to-cart" data-art="'+key+'">Купить</button>';
 			out+='</div>';
-		//console.log(data);
 		}
-
-		$('#goods').html(out);
-
+	$('#goods').html(out);
+	$('button.add-to-cart').on('click', addToCart);
 	});
+}
+
+function addToCart(){
+		//добавляем товар в корзину
+		var articul = $(this).attr('data-art');
+		if (cart[articul]!=undefined) {
+			cart[articul]++;
+		}
+		else {
+			cart[articul] = 1;
+		}
+		                               //масив с которым рабатоем который преобразовуем в строку для локал сторидж
+		localStorage.setItem('cart', JSON.stringify(cart) );
+	//console.log(cart);
+	showMiniCart();
+
+}
+
+function checkCart() {
+	//проверяем наличие корзины в localStorage;
+	//console.log ( localStorage.getItem('dddd') );
+	if ( localStorage.getItem('cart') != null ) {
+		cart = JSON.parse (localStorage.getItem('cart'));
+	}
+}
+
+function showMiniCart() {
+	//показываю содержимое корзины
+
+	var out = '';
+	for (var w in cart){
+		out += w + ' --- '+cart[w]+'<br>';
+	}
+	$('#mini-cart').html(out);
 }
