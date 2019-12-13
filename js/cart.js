e8cad190b8d0cf372520c69b8a0001dfc74322f5
YuriiTovmach <portfolio_yurii_tovmach@ukr.net>
2019-12-13 19:14:05 +0200

    fifth commit

--- js/cart.js
+++ js/cart.js
@@ -1,74 +1,74 @@
-let cart = {}; //корзина
+var cart = {}; //корзина
 
-$.getJSON('goods.json', function(data){
 
-	let goods = data; //все товары в масиве
-	//console.log(goods);
-	checkCart();
-	//console.log(cart);
-	showCart(); //вывожу товары на страницу
+$.getJSON('goods.json', function (data) {
+    var goods = data; //все товары в массиве
+    // console.log(goods);
+    checkCart();
+    //console.log(cart);
+    showCart(); //вывожу товары на страницу
 
-	function showCart(){
-		if ( $.isEmptyObject(cart) ) {
-			//корзина пуста
-			let out = 'Корзина пуста. Добавте товар в корзину <a href="index.html">главная страница</a>';
-			$('#my-cart').html(out);
-		}
-		else {
-		let out = '';
-		for (let key in cart){
-			//out+= key + '---'+cart[key]+'<br>'; //выводим индексы и количество товаров
-			out += '<button class="delete" data-art="'+key+'">x</button>';
-			out += '<img src="'+goods[key].image+'" width="48">';
-			out += goods[key].name;
-			out += '<button class="minus" data-art="'+key+'">-</button>';
-			out += cart[key];
-			out += '<button class="plus" data-art="'+key+'">+</button>';
-			out += cart[key]*goods[key].cost;
-			out += '<br>';
-		}
-		$('#my-cart').html(out);
-		$('.plus').on('click', plusGoods);
-		$('.minus').on('click', minusGoods);
-		$('.delete').on('click', deleteGoods);
+    function showCart() {
+        if ($.isEmptyObject(cart)) {
+            //корзина пуста
+            var out = 'Корзина пуста. Добавьте товар в корзину <a href="/eshop.com/">главная страница</a>';
+            $('#my-cart').html(out);
+        }
+        else {
+            var out = '';
+            for (var key in cart) {
+                out += '<button class="delete" data-art="' + key + '" >x</button>';
+                out += '<img src="' + goods[key].image + '" width="48">';
+                out += goods[key].name;
+                out += '<button class="minus" data-art="' + key + '">-</button>';
+                out += cart[key];
+                out += '<button class="plus" data-art="' + key + '">+</button>';
+                out += cart[key] * goods[key].cost;
+                out += '<br>';
+            }
+            $('#my-cart').html(out);
+            $('.plus').on('click', plusGoods);
+            $('.minus').on('click', minusGoods);
+            $('.delete').on('click', deleteGoods);
+        }
+    }
 
-		}	
-	}
-	function plusGoods(){
-		let articul = $(this).attr('data-art');
-		cart[articul]++;
-		saveCartToLS(); //сохраняем корзину в LocalStorage
-		showCart();
-		}
+    function plusGoods() {
+        var articul = $(this).attr('data-art');
+        cart[articul]++;
+        saveCartToLS(); //сохраняю корзину в localStorage
+        showCart();
+    }
 
-	function minusGoods(){
-		let articul = $(this).attr('data-art');
-		if (cart[articul]>1) {
-			cart[articul]--; 
-		}
-		else { 
-			delete cart[articul];
-		}
-		saveCartToLS(); //сохраняем корзину в LocalStorage
-		showCart();
-		}
+    function minusGoods() {
+        var articul = $(this).attr('data-art');
+        if (cart[articul] > 1) {
+            cart[articul]--;
+        }
+        else {
+            delete cart[articul];
+        }
+        saveCartToLS();//сохраняю корзину в localStorage
+        showCart();
+    }
 
-	function deleteGoods(){
-		let articul = $(this).attr('data-art');
-		delete cart[articul];
-		saveCartToLS(); //сохраняем корзину в LocalStorage
-		showCart();
-		}		
+    function deleteGoods() {
+        var articul = $(this).attr('data-art');
+        delete cart[articul];
+        saveCartToLS();//сохраняю корзину в localStorage
+        showCart();
+    }
+
+
 });
 
 function checkCart() {
-	//проверяем наличие корзины в localStorage;
-	//console.log ( localStorage.getItem('dddd') );
-	if ( localStorage.getItem('cart') != null ) {
-		cart = JSON.parse (localStorage.getItem('cart'));
-	}
+    //проверяю наличие корзины в localStorage;
+    if (localStorage.getItem('cart') != null) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
 }
 
-function saveCartToLS(){
-	localStorage.setItem('cart', JSON.stringify(cart) );
+function saveCartToLS() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
